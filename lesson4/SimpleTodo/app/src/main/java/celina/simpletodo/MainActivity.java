8a47426df29b8c140317d7b3a492ae9f61a4ea33
songbird175 <celina.bekins@students.olin.edu>
2016-11-16 17:03:27 -0500

    Getting started on the tutorial

--- lesson4/SimpleTodo/app/src/main/java/celina/simpletodo/MainActivity.java
+++ lesson4/SimpleTodo/app/src/main/java/celina/simpletodo/MainActivity.java
@@ -2,6 +2,7 @@ package celina.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
